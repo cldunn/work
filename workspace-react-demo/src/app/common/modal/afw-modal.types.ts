dbdbdab56387579f6dcde80cbbe4bd480c3c38fa
cldunn <cliffdunntx@yahoo.com>
2021-08-22 15:25:35 -0500

    Remove original demos and simple-mvc

--- workspace-react-demo/src/app/common/modal/afw-modal.types.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-import { ModalProps } from "react-bootstrap";
-
-export interface AfwModalProps extends ModalProps {
-    show: boolean;
-    closeModal: boolean;
-    children: ReactNode;
-    dialogClassName: string;
-}
